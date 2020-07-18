0e16b72c3f7d6a34c36723926e0992b6b44b4684
MrCaro <36524539+MrCaro@users.noreply.github.com>
2020-07-17 20:38:10 -0400

    Updates

---
+++ precache-manifest.b1c856cec0ec6ea49f350c9a99965566.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6286c0e4c77f7d91f3ec028e1fc8cd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3cc448f51ac7bca30d",
+    "url": "/static/css/main.d3f9e8d3.chunk.css"
+  },
+  {
+    "revision": "cac317914757fbf4aa72",
+    "url": "/static/js/2.f484146c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f484146c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3cc448f51ac7bca30d",
+    "url": "/static/js/main.458d0a52.chunk.js"
+  },
+  {
+    "revision": "26d75c173d80194105ca",
+    "url": "/static/js/runtime-main.c2349923.js"
+  },
+  {
+    "revision": "c73b8a9c6607f7a34178cd2d5b6675b9",
+    "url": "/static/media/Anim-logo–1.1.c73b8a9c.png"
+  },
+  {
+    "revision": "455505859532b55bd8c7f85b47a39ba9",
+    "url": "/static/media/Resume-CarlosCaro-2020.45550585.pdf"
+  },
+  {
+    "revision": "8a9502acb830a9ba622d0fac3390f9ce",
+    "url": "/static/media/artboard–1.8a9502ac.png"
+  },
+  {
+    "revision": "14b67141ec7b565f1963fd29577bcc25",
+    "url": "/static/media/carlosBg3.14b67141.png"
+  },
+  {
+    "revision": "a4b8d4800ad9aca01b7cd67f9d55dbd1",
+    "url": "/static/media/iPhoneX-mockup.a4b8d480.png"
+  },
+  {
+    "revision": "3de8c0d2d8c36dc7d7b644cc0dfa5fc8",
+    "url": "/static/media/iconBig-3d.3de8c0d2.png"
+  },
+  {
+    "revision": "09c50007c323f10f97ed04fe8259942a",
+    "url": "/static/media/iconBig-code.09c50007.png"
+  },
+  {
+    "revision": "b80e2b58d6f2691f7c64e17e229abfe5",
+    "url": "/static/media/iconBig-ui.b80e2b58.png"
+  },
+  {
+    "revision": "02597fc71abcf55df7f271c3b32c3a02",
+    "url": "/static/media/test-logo-anim-5.02597fc7.gif"
+  }
+]);
