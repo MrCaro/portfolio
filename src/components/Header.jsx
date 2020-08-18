bc87641dd3dc856ca11055e3706b76c5e73a6319
MrCaro <36524539+MrCaro@users.noreply.github.com>
2020-08-18 17:20:31 -0400

    adding hover effect on skill icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Icons from '../assets/js/useIcons';
 import Logo from '../assets/img/newIcon-CC.svg';
-import htmlIcon from '../assets/img/icon-html5.svg';
-import reactIcon from '../assets/img/icon-react.svg';
-import cssIcon from '../assets/img/icon-css3.svg';
-import gitIcon from '../assets/img/icon-git.svg';
-import jsIcon from '../assets/img/icon-js.svg';
+import htmlIcon from '../assets/img/icon-html5-color.svg';
+import reactIcon from '../assets/img/icon-react-color.svg';
+import cssIcon from '../assets/img/icon-css-color.svg';
+import gitIcon from '../assets/img/icon-git-color.svg';
+import jsIcon from '../assets/img/icon-js-color.svg';
 
 //TODO add hover for tech skills svg's
 //TODO resize tech skills svg to 40px maybe
@@ -22,11 +22,11 @@ const Header = () => {
                 </div>
             </div>
             <div className="flex flex-row flex-wrap justify-center mb-16">
-                <img className="programming-icons m-3" src={htmlIcon} alt=""/>
-                <img className="programming-icons m-3" src={reactIcon} alt=""/>
-                <img className="programming-icons m-3" src={cssIcon} alt=""/>
-                <img className="programming-icons m-3" src={gitIcon} alt=""/>
-                <img className="programming-icons m-3" src={jsIcon} alt=""/>
+                <img className="programming-icons w-8 m-3 sm:mx-6" src={htmlIcon} alt=""/>
+                <img className="programming-icons w-8 m-3 sm:mx-6" src={reactIcon} alt=""/>
+                <img className="programming-icons w-8 m-3 sm:mx-6" src={cssIcon} alt=""/>
+                <img className="programming-icons w-8 m-3 sm:mx-6" src={gitIcon} alt=""/>
+                <img className="programming-icons w-8 m-3 sm:mx-6" src={jsIcon} alt=""/>
             </div>
             <div className="grid grid-cols-1 mb-16" style={{justifyItems: "center"}}>
                 <div className="theme-switch-wrapper">
